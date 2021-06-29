380ad7feabc504cad75e5388648dee121364bf17
NguyenLong <nguyentienlong147@gmail.com>
2021-06-29 23:48:50 +0700

    .gitignore is now working

---
+++ CoralCivet-Technology-Ecommerce-Website/CoralCivet-Technology-Ecommerce-Website/Areas/Admin/Controllers/AdminIndexController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoralCivet_Technology_Ecommerce_Website.Areas.Admin.Controllers
+{
+    public class AdminIndexController : Controller
+    {
+        // GET: Admin/AdminIndex
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
