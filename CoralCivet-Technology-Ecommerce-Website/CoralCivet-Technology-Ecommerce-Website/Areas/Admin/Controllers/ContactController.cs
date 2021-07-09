26434e6dacff862d76a039d8792b186928d0bd37
nguyenlehongvi <lehongvi2000@gmail.com>
2021-07-09 11:15:56 +0700

    layout

---
+++ CoralCivet-Technology-Ecommerce-Website/CoralCivet-Technology-Ecommerce-Website/Areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoralCivet_Technology_Ecommerce_Website.Areas.Admin.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Admin/Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
