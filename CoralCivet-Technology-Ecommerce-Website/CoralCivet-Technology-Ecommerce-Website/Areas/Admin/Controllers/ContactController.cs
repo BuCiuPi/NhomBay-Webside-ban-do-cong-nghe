497a6b70e9b8abf0c2e8d799fa97fdd972062314
nguyenlehongvi <lehongvi2000@gmail.com>
2021-07-24 17:05:11 +0700

    contact - 50%

--- CoralCivet-Technology-Ecommerce-Website/CoralCivet-Technology-Ecommerce-Website/Areas/Admin/Controllers/ContactController.cs
+++ CoralCivet-Technology-Ecommerce-Website/CoralCivet-Technology-Ecommerce-Website/Areas/Admin/Controllers/ContactController.cs
@@ -1,17 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CoralCivet_Technology_Ecommerce_Website.Models.CoralCivet;
+using PagedList;
+using System;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace CoralCivet_Technology_Ecommerce_Website.Areas.Admin.Controllers
 {
     public class ContactController : Controller
     {
+        CoralCivetContext db = new CoralCivetContext();
         // GET: Admin/Contact
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
-            return View();
+            ViewBag.Count = db.contacts.Count();
+            return View(db.contacts.OrderByDescending(n => n.updated_at).ToPagedList(page ?? 1, 20));
+        }
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            contact contact = db.contacts.Find(id);
+            db.contacts.Remove(contact);
+            db.SaveChanges();
+            TempData["Notification"] = String.Format("Xóa chủ đề liên hệ thành công.");
+            return RedirectToAction("Index");
         }
     }
 }
