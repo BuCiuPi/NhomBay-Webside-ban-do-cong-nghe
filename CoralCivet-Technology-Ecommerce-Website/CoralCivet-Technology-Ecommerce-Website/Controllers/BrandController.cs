b4ff9e6fd394c261c9370af05230c75c87ea90b4
NguyenLong <nguyentienlong147@gmail.com>
2021-07-24 21:10:55 +0700

    sua nav bar

---
+++ CoralCivet-Technology-Ecommerce-Website/CoralCivet-Technology-Ecommerce-Website/Controllers/BrandController.cs
@@ -0,0 +1,25 @@
+﻿using CoralCivet_Technology_Ecommerce_Website.Models.CoralCivet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoralCivet_Technology_Ecommerce_Website.Controllers
+{
+    public class BrandController : Controller
+    {
+        // GET: Brand
+        CoralCivetContext context = new CoralCivetContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult GetBrandList()
+        {
+            return View("_HeaderBands",context.Brands.Where(p =>p.name.Equals("NO BRAND")).ToList());
+        }
+    }
+}
